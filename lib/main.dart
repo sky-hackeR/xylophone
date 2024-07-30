aadf06c1c9525133bff8f0faf0bdc16535b32ad6
skyhackeR <jolayemidavid6@gmail.com>
2024-07-30 15:51:54 +0100

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,57 @@
-import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+// import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(const XylophoneApp());
+// void main() => runApp(const XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({super.key});
+// class XylophoneApp extends StatelessWidget {
+//   const XylophoneApp({super.key});
 
-  void playSound(int soundNumber) {
+//   void playSound(int soundNumber) {
 
-    final player = AudioPlayer();
+//     final player = AudioPlayer();
                   
-    player.play(AssetSource('note$soundNumber.wav'));
+//     player.play(AssetSource('note$soundNumber.wav'));
 
-  }
+//   }
 
-  Expanded buildKey({required Color color, required int soundNumber}) {
-    return Expanded(
-      child: TextButton(
-        onPressed: (){
-          playSound(soundNumber);
-        }, 
-        style: TextButton.styleFrom(
-          backgroundColor: color, 
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.zero,
-          ), 
-        ),
-        child: Container(),
-      ),
-    );
-  }
+//   Expanded buildKey({required Color color, required int soundNumber}) {
+//     return Expanded(
+//       child: TextButton(
+//         onPressed: (){
+//           playSound(soundNumber);
+//         }, 
+//         style: TextButton.styleFrom(
+//           backgroundColor: color, 
+//           shape: const RoundedRectangleBorder(
+//             borderRadius: BorderRadius.zero,
+//           ), 
+//         ),
+//         child: Container(),
+//       ),
+//     );
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              buildKey(color: Colors.purple, soundNumber: 1),
-              buildKey(color: Colors.pink, soundNumber:2),
-              buildKey(color: Colors.blue, soundNumber:3),
-              buildKey(color: Colors.orange, soundNumber:4),
-              buildKey(color: Colors.brown, soundNumber:5),
-              buildKey(color: Colors.yellow, soundNumber:6),
-              buildKey(color: Colors.red, soundNumber:7),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.black,
+//         body: SafeArea(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: <Widget>[
+//               buildKey(color: Colors.purple, soundNumber: 1),
+//               buildKey(color: Colors.pink, soundNumber:2),
+//               buildKey(color: Colors.blue, soundNumber:3),
+//               buildKey(color: Colors.orange, soundNumber:4),
+//               buildKey(color: Colors.brown, soundNumber:5),
+//               buildKey(color: Colors.yellow, soundNumber:6),
+//               buildKey(color: Colors.red, soundNumber:7),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
  
